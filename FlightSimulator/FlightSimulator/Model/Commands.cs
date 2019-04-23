4b31d2504d83f4bd40429214b0c0f82aedcf0408
gilKag2 <35850376+gilKag2@users.noreply.github.com>
2019-04-23 15:46:44 +0300

    client server update

---
+++ FlightSimulator/FlightSimulator/Model/Commands.cs
@@ -0,0 +1,50 @@
+﻿using FlightSimulator.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Model
+{
+    //singleton.
+    public sealed class Commands
+    {
+        private TcpClient _client;
+        private BinaryWriter _writer;
+        private ISettingsModel _settings;
+        private static Commands _instance = null;
+        private static readonly object padLock = new object();
+        public static Commands Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Commands();
+                return _instance;
+            }
+        }
+        public Commands()
+        {
+            _settings = ApplicationSettingsModel.Instance;
+        }
+        public void Connect()
+        {
+            _client = new TcpClient();
+
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(_settings.FlightServerIP), _settings.FlightCommandPort);
+            while (!_client.Connected)
+            {
+                try
+                {
+                    _client.Connect(ep);
+                } catch (Exception) { }
+            }
+        }
+
+
+    }
+}
---
+++ FlightSimulator/FlightSimulator/Model/Info.cs
@@ -0,0 +1,50 @@
+﻿using FlightSimulator.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Model
+{
+    public sealed class Info
+    {
+        private TcpListener server;
+        private TcpClient client;
+        private BinaryReader reader;
+        private ISettingsModel _settings;
+        private static Info _instance;
+        private static readonly object padLock = new object();
+        public static Info Instance {
+            get
+            {
+                // thread safety.
+                lock (padLock)
+                {
+                    if (_instance == null)
+                        _instance = new Info();
+                    return _instance;
+                }
+            }
+    }
+        public Info()
+        {
+            _settings = ApplicationSettingsModel.Instance;
+        }
+        public void closeServer()
+        {
+            client.Close();
+            server.Stop();
+        }
+
+        public void openServer()
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(_settings.FlightServerIP), _settings.FlightInfoPort);
+            if (server != null) closeServer();
+            server = new TcpListener(ep);
+            server.Start();
+        }
+}
